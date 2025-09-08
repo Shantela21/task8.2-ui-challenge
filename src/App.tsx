e32427d2ac16cb1e9388c33d8549b49236ffe090
Shantela21 <shantelaslie@gmail.com>
2025-09-08 09:18:51 +0200

    added read me

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "./assets/images/logo.svg";
+import Dropdown from "./components/dropdown";
+import menu from "./assets/images/icon-menu.svg";
+import databiz from "./assets/images/client-databiz.svg";
+import audiophile from "./assets/images/client-audiophile.svg";
+import meet from "./assets/images/client-meet.svg";
+import maker from "./assets/images/client-maker.svg";
+import herodesktop from "./assets/images/image-hero-desktop.png";
+import heromobile from "./assets/images/image-hero-mobile.png";
+import { Button1, Button2 } from "./components/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavSection />
+      <BottomSection />
     </>
-  )
+  );
 }
 
-export default App
+function NavSection() {
+  return (
+    <div id="nav">
+      <div id="Nav-Left">
+        <img className="mobile-desktop" src={logo} alt="logo" />
+        <div className="desktop" id="drop-downs">
+          <Dropdown name="Features" />
+          <Dropdown name="Company" />
+          {/* <Dropdown name="Careers" />
+          <Dropdown name="About" /> */}
+          <p>Careers</p>
+          <p>About</p>
+        </div>
+      </div>
+      <div id="Nav-right">
+        <p className="desktop">Login</p>
+        <Button1 name="Register" /> 
+        <img className="mobile" src={menu} alt="menu" />
+      </div>
+    </div>
+  );
+}
+
+function BottomSection() {
+  return (
+    <div id="bottom-section">
+      <div className="mobile-desktop main-bottom-section">
+        <div className="desktop" id="bottom-section-left-desktop">
+          <div id="text-content">
+            <h1>
+              Make <br /> remote work
+            </h1>
+            <p>
+              Get your team in sync ,no matter your location
+              <br />
+              Streamline processes,create team rituals,and <br />
+              watch productivity soar
+            </p>
+            <Button2 name="Learn more" />
+          </div>
+          <div id="bottom-images">
+            <img src={databiz} alt="databiz" />
+            <img src={audiophile} alt="audiophile" />
+            <img src={meet} alt="meet" />
+            <img src={maker} alt="maker" />
+          </div>
+        </div>
+        <div
+          className="desktop"
+          style={{
+            backgroundImage: `url(${herodesktop})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+            width: "50%",
+          }}
+        ></div>
+
+        <div className="mobile" id="bottom-section-left-mobile">
+          <img className="mobile mobile-hero" src={heromobile} alt="hero" />
+          <div id="text-content">
+            <h1>Make remote work</h1>
+            <p>
+              Get your team in sync ,no matter your location
+              <br />
+              Streamline processes, create team rituals,and <br />
+              watch productivity soar
+            </p>
+            <Button2 name="Learn more" />
+          </div>
+          <div id="bottom-images">
+            <img src={databiz} alt="databiz" />
+            <img src={audiophile} alt="audiophile" />
+            <img src={meet} alt="meet" />
+            <img src={maker} alt="maker" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+export function Button1({ name }) {
+  return (
+    <>
+      <button className="button1 desktop">{name}</button>
+    </>
+  );
+}
+
+export function Button2({ name }) {
+  return (
+    <>
+      <button className="button2">{name}</button>
+    </>
+  );
+}
